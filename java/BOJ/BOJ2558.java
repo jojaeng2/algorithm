114db7dfea8ca2db2c2dc82fe66e0ef7bb91fe03
jojaeng2 <ds4ouj@naver.com>
2021-10-17 11:09:28 +0900

    Create BOJ2558.java

---
+++ java/BOJ/BOJ2558.java
@@ -0,0 +1,12 @@
+package first;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt(), num2 = sc.nextInt();
+		System.out.println(num1+num2);
+	}
+}
+
